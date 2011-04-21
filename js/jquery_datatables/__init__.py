8d2eaeec997aa0e00f97eafc8d893783e6fb25fd
Izhar Firdaus <kagesenshi.87@gmail.com>
2011-04-21 17:08:33 +0800

    initial import

---
+++ js/jquery_datatables/__init__.py
@@ -0,0 +1,14 @@
+from fanstatic import Library, Resource
+from js.jquery import jquery
+
+library = Library('jquery_datatables', 'resources')
+
+# Define the resources in the library like this.
+# For options and examples, see the fanstatic documentation.
+# resource1 = Resource(library, 'style.css')
+
+datatables = Resource(
+    datatables_lib, 'media/js/jquery.dataTables.js',
+    depends=[jquery],
+    minified='media/js/jquery.dataTables.min.js'
+)
