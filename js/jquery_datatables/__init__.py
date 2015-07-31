c01ebd15ef483d296d0c823ea64a08b6648e0973
Sylvain Viollon <sviollon@minddistrict.com>
2015-07-31 10:02:57 +0200

    Update to 1.10.7

--- js/jquery_datatables/__init__.py
+++ js/jquery_datatables/__init__.py
@@ -23,73 +23,72 @@ jquery_datatables = Group(depends=[
 )
 
 autofill_css = Resource(
-    library, 'extras/AutoFill/css/dataTables.autoFill.css')
+    library, 'extensions/AutoFill/css/dataTables.autoFill.css')
 
 autofill = Resource(
-    library, 'extras/AutoFill/js/dataTables.autoFill.js',
+    library, 'extensions/AutoFill/js/dataTables.autoFill.js',
     depends=[jquery_datatables_js, autofill_css],
-    minified='extras/AutoFill/js/dataTables.autoFill.min.js')
+    minified='extensions/AutoFill/js/dataTables.autoFill.min.js')
 
 colreorder_css = Resource(
-    library, 'extras/ColReorder/css/dataTables.colReorder.css')
+    library, 'extensions/ColReorder/css/dataTables.colReorder.css')
 
 colreorder = Resource(
-    library, 'extras/ColReorder/js/dataTables.colReorder.js',
+    library, 'extensions/ColReorder/js/dataTables.colReorder.js',
     depends=[jquery_datatables_js, colreorder_css],
-    minified='extras/ColReorder/js/dataTables.colReorder.min.js')
+    minified='extensions/ColReorder/js/dataTables.colReorder.min.js')
 
 colvis_css = Resource(
-    library, 'extras/ColVis/css/dataTables.colVis.css')
+    library, 'extensions/ColVis/css/dataTables.colVis.css')
 
 colvis_jqueryui_css = Resource(
-    library, 'extras/ColVis/css/dataTables.colvis.jqueryui.css')
+    library, 'extensions/ColVis/css/dataTables.colvis.jqueryui.css')
 
 colvis = Resource(
-    library, 'extras/ColVis/js/dataTables.colVis.js',
+    library, 'extensions/ColVis/js/dataTables.colVis.js',
     depends=[jquery_datatables_js, colvis_css],
-    minified='extras/ColVis/js/dataTables.colVis.min.js')
+    minified='extensions/ColVis/js/dataTables.colVis.min.js')
 
 fixed_columns = Resource(
-    library, 'extras/FixedColumns/js/dataTables.fixedColumns.js',
+    library, 'extensions/FixedColumns/js/dataTables.fixedColumns.js',
     depends=[jquery_datatables_js],
-    minified='extras/FixedColumns/js/dataTables.fixedColumns.min.js')
+    minified='extensions/FixedColumns/js/dataTables.fixedColumns.min.js')
 
 fixed_header = Resource(
-    library, 'extras/FixedHeader/js/dataTables.fixedHeader.js',
+    library, 'extensions/FixedHeader/js/dataTables.fixedHeader.js',
     depends=[jquery_datatables_js],
-    minified='extras/FixedHeader/js/dataTables.fixedHeader.min.js')
+    minified='extensions/FixedHeader/js/dataTables.fixedHeader.min.js')
 
 keytable_css = Resource(
-    library, 'extras/KeyTable/css/dataTables.keyTable.css')
+    library, 'extensions/KeyTable/css/dataTables.keyTable.css')
 
 keytable = Resource(
-    library, 'extras/KeyTable/js/dataTables.keyTable.js',
+    library, 'extensions/KeyTable/js/dataTables.keyTable.js',
     depends=[jquery_datatables_js, keytable_css],
-    minified='extras/KeyTable/js/dataTables.keyTable.min.js')
+    minified='extensions/KeyTable/js/dataTables.keyTable.min.js')
 
 responsive_css = Resource(
-    library, 'extras/Responsive/css/dataTables.responsive.css')
+    library, 'extensions/Responsive/css/dataTables.responsive.css')
 
 responsive = Resource(
-    library, 'extras/Responsive/js/dataTables.responsive.js',
+    library, 'extensions/Responsive/js/dataTables.responsive.js',
     depends=[jquery_datatables_js, responsive_css],
-    minified='extras/Responsive/js/dataTables.responsive.min.js')
+    minified='extensions/Responsive/js/dataTables.responsive.min.js')
 
 scroller_css = Resource(
-    library, 'extras/Scroller/css/dataTables.scroller.css')
+    library, 'extensions/Scroller/css/dataTables.scroller.css')
 
 scroller = Resource(
-    library, 'extras/Scroller/js/dataTables.scroller.js',
+    library, 'extensions/Scroller/js/dataTables.scroller.js',
     depends=[jquery_datatables_js, scroller_css],
-    minified='extras/Scroller/js/dataTables.scroller.min.js')
+    minified='extensions/Scroller/js/dataTables.scroller.min.js')
 
 table_tools_css = Resource(
-    library, 'extras/TableTools/css/dataTables.tableTools.css')
+    library, 'extensions/TableTools/css/dataTables.tableTools.css')
 
 table_tools_js = Resource(
-    library, 'extras/TableTools/js/dataTables.tableTools.js',
-    minified='extras/TableTools/js/dataTables.tableTools.min.js',
+    library, 'extensions/TableTools/js/dataTables.tableTools.js',
+    minified='extensions/TableTools/js/dataTables.tableTools.min.js',
     depends=[jquery_datatables_js, table_tools_css])
 
 table_tools = Group(depends=[table_tools_js, table_tools_css])
-
